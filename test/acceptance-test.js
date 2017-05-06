43d86a0d456229bd11db11e3607c4d3c0fa69d9e
Kelly Selden <kellyselden@gmail.com>
2017-05-06 13:12:09 -0700

    es6

--- test/acceptance-test.js
+++ test/acceptance-test.js
@@ -1,26 +1,28 @@
-var expect = require('chai').expect;
-var RSVP = require('rsvp');
-var request = RSVP.denodeify(require('request'));
-var AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
+'use strict';
+
+const expect = require('chai').expect;
+const RSVP = require('rsvp');
+const request = RSVP.denodeify(require('request'));
+const AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
 
 describe('Acceptance | ember-cli-build', function() {
   this.timeout(300000);
 
-  var app;
+  let app;
 
   before(function() {
     app = new AddonTestApp();
 
     return app.create('dummy', {
       fixturesPath: 'tests'
-    }).then(function() {
+    }).then(() => {
       return app.run(
         'npm',
         'install',
         '--save-dev',
         `broccoli-funnel@${process.env.npm_package_devDependencies_broccoli_funnel}`
       );
-    }).then(function() {
+    }).then(() => {
       return app.startServer();
     });
   });
@@ -31,7 +33,7 @@ describe('Acceptance | ember-cli-build', function() {
 
   it('can load the package', function() {
     return request('http://localhost:49741/assets/vendor.js')
-      .then(function(response) {
+      .then(response => {
         expect(response.body).to.contain('The string works!');
         expect(response.body).to.contain('The funnel works!');
       });
