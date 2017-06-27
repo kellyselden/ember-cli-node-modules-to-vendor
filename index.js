e68ba3e364ad51b5b5e06197a617fa26f35ee2cb
Jason Mitchell <jasonmit@nestlabs.com>
2017-06-27 13:55:19 -0700

    Prevent ember-cli watcher being assigned to node_modules

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 'use strict';
 
 const mergeTrees = require('broccoli-merge-trees');
+const UnwatchedDir = require('broccoli-source').UnwatchedDir;
 
 module.exports = {
   name: 'ember-cli-node-modules-to-vendor',
 
   included(app) {
+    this._super.included.apply(this, arguments);
     this.app = app;
   },
 
   treeForVendor() {
     let app = this.app;
     let nodeModulesToVendor = app.options.nodeModulesToVendor || [];
-    return mergeTrees(nodeModulesToVendor);
+
+    return mergeTrees(nodeModulesToVendor.map(function(asset) {
+      if (typeof asset === 'string') {
+        return new UnwatchedDir(asset);
+      }
+
+      return asset;
+    }));
   }
 };
